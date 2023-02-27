25d0d494ec71a6ddb895d5363cf0b69f74312f9a
wying <wying@qq.com>
2023-02-27 14:11:58 +0800

    master commit

--- haha.java
+++ haha.java
@@ -5,5 +5,6 @@ public class haha {
         System.out.println("hello world");
         System.out.println("hello git");
         System.out.println("hello world");
+        System.out.println("hello master");
     }
 }
