7d1a6cd41a7c4c4ffce8e024254c7233a87ff278
wying <wying@qq.com>
2023-02-27 14:38:37 +0800

    text commit

--- haha.java
+++ haha.java
@@ -8,5 +8,6 @@ public class haha {
         System.out.println("hello hot-fix");
         System.out.println("hello hot-fix2");
         System.out.println("hello master");
+        System.out.println("test");
     }
 }
